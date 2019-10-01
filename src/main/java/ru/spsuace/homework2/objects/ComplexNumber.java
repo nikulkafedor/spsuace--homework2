def73d7f59afa4f48365ebc274fc618fc3662b45
Veronika <veronikafed@mail.ru>
2019-10-02 01:03:07 +0300

    done

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -5,4 +5,49 @@ package ru.spsuace.homework2.objects;
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+    private final int dReal, dImaginary;
+
+    public ComplexNumber( int dReal, int dImaginary) {
+        this.dReal = dReal;
+        this.dImaginary = dImaginary;
+    }
+    public int getReal() {
+        return dReal;
+    }
+    public int getComplex() {
+        return dImaginary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ComplexNumber that = (ComplexNumber) o;
+        return dReal == that.dReal && dImaginary == that.dImaginary;
+    }
+
+    @Override
+    public int hashCode() {
+        return Math.abs(this.dReal) + Math.abs(this.dImaginary);
+    }
+
+    @Override
+    public String toString() {
+        if (this.dImaginary < 0) {
+            return this.dReal + " - " + Math.abs(this.dImaginary) + "i";
+        } else {
+            return this.dReal + " + " + this.dImaginary + "i";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ComplexNumber(11, -24));
+    }
 }
+
+
+
